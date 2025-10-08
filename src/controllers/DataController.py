39c0701b45caca759ba113fad5cb49790f83bd4a
amrkhalid <muslimidentity12@gmail.com>
2025-10-08 14:59:38 +0300

    feat: implement file upload functionality with unique path generation

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -2,11 +2,14 @@ from fastapi import UploadFile
 from helpers.config import Settings, get_settings
 from models import ResponseSignal
 from .BaseController import BaseController
-
+from .ProjectController import ProjectController
+import re
+import os
 
 class DataController(BaseController):
     def __init__(self):
         self.size_scale = 1048576  # 1 MB in bytes
+        self.app_settings = get_settings()  # <-- Add this line
         super().__init__()
 
 
@@ -21,3 +24,39 @@ class DataController(BaseController):
         if file.content_type not in get_settings().FILE_ALLOWED_TYPES :
             return False, ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
         return True ,ResponseSignal.FILE_UPLOAD_SUCCESS.value
+    
+    
+    def generate_unique_filepath(self, orig_file_name: str, project_id: str):
+
+        random_key = self.generate_random_string()
+        project_path = ProjectController().get_project_path(project_id=project_id)
+
+        cleaned_file_name = self.get_clean_file_name(
+            orig_file_name=orig_file_name
+        )
+
+        new_file_path = os.path.join(
+            project_path,
+            random_key + "_" + cleaned_file_name
+        )
+
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.join(
+                project_path,
+                random_key + "_" + cleaned_file_name
+            )
+
+        return new_file_path, random_key + "_" + cleaned_file_name
+    def get_clean_file_name(self, orig_file_name: str):
+
+        # remove any special characters, except underscore and .
+        cleaned_file_name = re.sub(r'[^\w.]', '', orig_file_name.strip())
+
+        # replace spaces with underscore
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+
+        return cleaned_file_name
+
+
+
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,35 +1,52 @@
 # this api to upload data itself ;
-from fastapi import FastAPI , Depends , APIRouter , UploadFile ,File, status
+from fastapi import FastAPI, Depends, APIRouter, UploadFile, File, status
 from fastapi.responses import JSONResponse
-from helpers import Settings , get_settings
-from controllers import DataController 
+from helpers import Settings, get_settings
+from controllers import DataController
 from controllers import ProjectController
 import aiofiles
 import os
+from models import ResponseSignal
+import logging
 
-data_router = APIRouter(prefix="/api/v1",tags=["api_v1" , "data"])
+data_router = APIRouter(prefix="/api/v1", tags=["api_v1", "data"])
+logger = logging.getLogger('uvicorn.error')
+
 
 @data_router.post("/upload/{project_id}")
 async def upload_data(
-    project_id:str , file: UploadFile = File(...)
-    ,app_settings:Settings =Depends(get_settings)):
-
-    is_valid , signal=await DataController().validate_uploaded_file(file=file)
+    project_id: str, file: UploadFile = File(...),
+    app_settings: Settings = Depends(get_settings)
+):
+    is_valid, signal = await DataController().validate_uploaded_file(file=file)
     if not is_valid:
         return JSONResponse(
-            status_code = status.HTTP_400_BAD_REQUEST,
-              content={
-            "signla" : signal}
-            )
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signl": signal
+            }
+        )
+
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = os.path.join(project_dir_path , file.filename)
-    async with aiofiles.open(file_path, "wb") as f:
-        while chunk := await file.read(app_settings.FILE_CHUNK_SIZE):
-            await f.write(chunk)
-    return JSONResponse(
-            status_code = status.HTTP_202_ACCEPTED,
-              content={
-            "signla" : signal}
-            )
+    file_path, file_id = DataController().generate_unique_filepath(
+        orig_file_name=file.filename,
+        project_id=project_id
+    )
 
-
+    try:
+        async with aiofiles.open(file_path, "wb") as f:
+            while chunk := await file.read(app_settings.FILE_CHUNK_SIZE):
+                await f.write(chunk)
+            return JSONResponse(
+                content={
+                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value
+            })
+              
+    except Exception as e:
+        logger.error(f"Error while uploading file: {e}")
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
+            }
+        )
